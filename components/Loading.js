8eff0efacaf8b02f8c8870dfe1c80c3ad8f332d5
cpbhai <harshvsingh.223@gmail.com>
2021-11-14 23:15:13 +0530

    1.0.7 prod

--- components/Loading.js
+++ components/Loading.js
@@ -1,21 +1,13 @@
 import Backdrop from "@mui/material/Backdrop";
 const Loading = ({ show }) => {
   //show may be undefined so used ternary ops
-  const handleDots = () => {
-    const who = document.getElementById("dots");
-    setInterval(() => {
-      if (who.innerText === "...") who.innerText = "";
-      who.innerText += ".";
-    }, 600);
-  };
-  if (show) handleDots();
   return (
     <Backdrop
       sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
       open={show ? true : false}
     >
       <h4 className="textCenter">
-        Loading<span id="dots"></span>
+        Loading...
         <br></br>Please Wait
       </h4>
     </Backdrop>
--- pages/about_us.js
+++ pages/about_us.js
@@ -10,20 +10,20 @@ const AboutUs = () => {
           We started Blashio with a vision to give more options to the people
           who looks for the beauticians, salons, makeup artists, and settle by
           choosing less optimal options, many times, because of less options of
-          professionals. Either they find the
-          salons/parlours/artists/beauticians nearby them or through previous
-          connections. What we do for them, is that, we provide the entire list
-          of salons/beauty parlours/makeup artists/beauticians with all details.
-          So that the people could get the details of them near them. And could
-          appoint the professionals as per best of their choice.<br></br>Another
-          problem is that the beauticians, professionals, makeup artists,
-          parolours get only limited clients which come to them either offline
-          or through previous connections. So, most of the time, their land up
-          making loss to their work and business by remaining idle, because of
-          no appointments or limited appointments. We provide such business
-          owners/freelancers/artists/beauticians/makeup artists more
-          opportunities to get and attract the client with their services. So
-          that their business could grow better.
+          professionals. Either they find the salons, parlours, artists,
+          beauticians nearby them or through previous connections. What we do
+          for them, is that, we provide the entire list of salons, beauty
+          parlours, makeup artists, beauticians with all details. So that the
+          people could get the details of them near them. And could appoint the
+          professionals as per best of their choice.
+          <br></br>Another problem is that the beauticians, professionals,
+          makeup artists, parolours get only limited clients which come to them
+          either offline or through previous connections. So, most of the time,
+          their land up making loss to their work and business by remaining
+          idle, because of no appointments or limited appointments. We provide
+          such business owners, freelancers, artists, beauticians, makeup
+          artists more opportunities to get and attract the client with their
+          services. So that their business could grow better.
         </p>
 
         <h5 className="aboutwhyFounded">
